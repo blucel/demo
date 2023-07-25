05b935cb0ef4b42e26976d5d7db3927f2fd87817
曹文丽 <caowenli@yuan-info.com>
2023-07-25 14:04:15 +0800

    fix:保存按钮添加loading效果

--- src/views/component/alarmManage/index.vue
+++ src/views/component/alarmManage/index.vue
@@ -2,13 +2,13 @@
  * @Author: 李佐宁 lizuoning@yuan-info.com
  * @Date: 2023-07-19 14:58:43
  * @LastEditors: 曹文丽 caowenli@yuan-info.com
- * @LastEditTime: 2023-07-25 14:00:56
+ * @LastEditTime: 2023-07-25 14:04:07
  * @FilePath: \safe-product-alarm-browser\src\views\component\alarmManage\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="ya-box">
-    <YaEditForm :formColumns="formColumns" @submit="handlerToSubmit"></YaEditForm>
+    <YaEditForm :formColumns="formColumns" @submit="handlerToSubmit" :loading="loading"></YaEditForm>
   </div>
 </template>
 <script lang="ts" setup>
