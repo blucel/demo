6abdb3509b85c70662b3df00b3b48551e49df00c
曹文丽 <caowenli@yuan-info.com>
2023-07-25 11:10:52 +0800

    fix: 更改http请求路径

--- src/api/alarmManage/index.ts
+++ src/api/alarmManage/index.ts
@@ -2,11 +2,11 @@
  * @Author: 曹文丽 caowenli@yuan-info.com
  * @Date: 2023-07-21 09:48:03
  * @LastEditors: 曹文丽 caowenli@yuan-info.com
- * @LastEditTime: 2023-07-21 11:15:15
+ * @LastEditTime: 2023-07-25 11:10:06
  * @FilePath: \safe-product-alarm-browser\src\api\alarmManage\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { http } from "../../utils/https";
+import { http } from "@/utils/https";
 /**
  * @description: 查询告警类型
  */
